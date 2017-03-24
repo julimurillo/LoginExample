e9c9ce7f8ba6e4f7feb8a41de0fecf89aa445188
julimurillo <j-mm15@hotmail.com>
2017-03-24 09:44:56 -0500

    Login Example

---
+++ models/user.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+
+var userSchema = mongoose.Schema({
+    local: {
+        email: String,
+        password: String
+    },
+    facebook: {
+        id: String,
+        token: String,
+        email: String,
+        name: String,
+    }
+}
+);
+
+userSchema.methods.validPassword= function(password){
+    return bcrypt.compareSync(password,this.local.password);
+}
+
+
+userSchema.methods.hashPassword= function(password){
+    var user=this;
+
+    bcrypt.hash(password,null,null,function(err,hash){
+        if(err)
+        return next(err);
+        user.local.password= hash;
+    });
+
+};
+
+module.exports= mongoose.model('User',userSchema);
